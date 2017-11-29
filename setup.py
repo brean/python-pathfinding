7eabb377da63d7b5904789f6eb652b20055b9c05
Andreas Bresser <andreasbresser@gmail.com>
2017-11-29 09:34:52 +0100

    Detailed information in setup.py for pypi

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="pathfinding",
+    description="Pathfinding algorithms (based on Pathfinding.JS)",
+    url="https://github.com/brean/python-pathfinding",
     version="0.0.1",
+    license="MIT",
     author="Andreas Bresser",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
-    )
+)
 

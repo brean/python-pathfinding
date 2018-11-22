8751269d754a531d4acf8e49dcf53a02c3e3988e
Andreas Bresser <Andreas.Bresser@dfki.de>
2018-11-22 15:57:10 +0100

    new version number to build new version for pypi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/brean/python-pathfinding",
-    version="0.0.2",
+    version="0.0.3",
     license="MIT",
     author="Andreas Bresser",
     packages=find_packages(),

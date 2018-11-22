5f7ab7c8a013da6bcd43e599b0b6ab893571d7e8
Andreas Bresser <self@andreasbresser.de>
2018-11-22 15:30:51 +0100

    Add long_description (from readme-file)

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="pathfinding",
     description="Pathfinding algorithms (based on Pathfinding.JS)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/brean/python-pathfinding",
     version="0.0.2",
     license="MIT",

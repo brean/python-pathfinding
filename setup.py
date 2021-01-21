255977d62601b620832b4d857b29147811eb9aef
Andreas Bresser <self@andreasbresser.de>
2021-01-21 11:02:42 +0100

    update version number to 1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/brean/python-pathfinding",
-    version="0.0.4",
+    version="1.0",
     license="MIT",
     author="Andreas Bresser",
     packages=find_packages(),

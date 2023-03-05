671cbc0135559d7329d8252e575efb85e379054c
Andreas Bresser <self@andreasbresser.de>
2023-03-05 21:41:32 +0100

    rename tests to start with 'test_'

--- test/test_csv_pandas.py
+++ test/test_csv_pandas.py
@@ -23,7 +23,8 @@ def _find(matrix):
     print('operations:', runs, 'path length:', len(path))
     print(grid.grid_str(path=path, start=start, end=end))
 
-    assert path == [(0, 0), (0, 1), (1, 1), (1, 2), (1, 3), (1, 4), (2, 4)]
+    assert [tuple(p) for p in path] ==\
+        [(0, 0), (0, 1), (1, 1), (1, 2), (1, 3), (1, 4), (2, 4)]
 
 
 def test_csv_pandas_str():
